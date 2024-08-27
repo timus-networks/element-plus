ec5fb71d3dc70ed8511897ec56f0c636c8ec3ef1
Serkan Konakcı <serkan.konakci@timusnetworks.com>
2024-08-27 08:49:47 +0300

    refactor: change package name to @timus-networks/element-plus

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,22 +2,26 @@
 
 set -e
 
+export TAG_VERSION=$(git describe --tags --abbrev=0 | sed 's/^v//')
+export GIT_HEAD=$(git rev-parse HEAD)
+
 pnpm i --frozen-lockfile
 pnpm update:version
 
 pnpm build
 
 cd dist/element-plus
-npm publish --provenance
-cd -
-
-cd internal/eslint-config
+# npm publish --provenance
 npm publish
 cd -
 
-cd internal/metadata
-pnpm build
-npm publish
-cd -
+# cd internal/eslint-config
+# npm publish
+# cd -
+#
+# cd internal/metadata
+# pnpm build
+# npm publish
+# cd -
 
 echo "✅ Publish completed"
